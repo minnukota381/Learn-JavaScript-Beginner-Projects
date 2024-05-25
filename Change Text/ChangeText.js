6a4f056a93c41509b3b7477f99d7c4fbcac289d1
minnukota381 <minnukota381@gmail.com>
2024-05-25 13:27:25 +0530

    update | JSP

---
+++ Change Text/ChangeText.js
@@ -0,0 +1,8 @@
+function ChangeText() {
+    let x = document.getElementById("text");
+    if (x.textContent === "Mr. Minnu") {
+        x.textContent = "Mrs. Minnu";
+    } else {
+        x.textContent = "Mr. Minnu";
+    }
+}
---
+++ Clock/Clock.js
@@ -0,0 +1,8 @@
+d=new Date();
+setInterval(TimerF, 1000);
+
+function TimerF(){
+    d=new Date();
+    document.getElementById("demo").innerHTML= 'Current Time: ' + d.getHours()
+        + ':' + d.getMinutes() + ':' + d.getSeconds();
+}
