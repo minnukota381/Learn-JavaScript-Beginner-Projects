5736abff37eef781070ec960db822d5081932b8f
minnukota381 <minnukota381@gmail.com>
2024-06-06 09:07:23 +0530

    update | Copy Paste

--- Copy Paste/CopyPaste.js
+++ Copy Paste/CopyPaste.js
@@ -1,24 +1,33 @@
-let a=document.getElementById("copyButton")
-a.addEventListener("click", function() {
-  var textArea = document.getElementById("textArea");
+const copyButton = document.getElementById("copyButton");
+const pasteButton = document.getElementById("pasteButton");
+const textArea = document.getElementById("textArea");
+
+copyButton.addEventListener("click", function() {
   textArea.select();
   document.execCommand("copy");
-  if(a.textContent==="Copy Text"){
-    a.innerText="Copied"
-    setTimeout(()=>{
-      if(a.textContent==="Copied"){
-        a.innerText="Copy Text"
+
+  if (copyButton.textContent === "Copy Text") {
+    copyButton.innerText = "Copied";
+    setTimeout(() => {
+      if (copyButton.textContent === "Copied") {
+        copyButton.innerText = "Copy Text";
       }
-    },3000)
+    }, 3000);
   }
-  
 });
 
-document.getElementById("pasteButton").addEventListener("click", function() {
-  var textArea = document.getElementById("textArea");
+pasteButton.addEventListener("click", function() {
   textArea.focus();
-  navigator.clipboard.readText().then(function(clipboardText) {
-    textArea.value += clipboardText;
-    
-  });
+  
+  if (navigator.clipboard) {
+    navigator.clipboard.readText()
+      .then(clipboardText => {
+        textArea.value += clipboardText;
+      })
+      .catch(err => {
+        console.error('Failed to read clipboard contents: ', err);
+      });
+  } else {
+    alert('Clipboard API not available');
+  }
 });
