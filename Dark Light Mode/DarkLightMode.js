dd4b0decefa05793e4d366db52a6dbbcfa6a0726
minnukota381 <minnukota381@gmail.com>
2024-06-06 19:47:54 +0530

    update | DL Mode

--- Dark Light Mode/DarkLightMode.js
+++ Dark Light Mode/DarkLightMode.js
@@ -1,9 +1,10 @@
-var icon = document.getElementById("icon");
-icon.addEventListener(('click'),() =>{
+let icon = document.getElementById("icon");
+
+icon.addEventListener('click', () => {
     document.body.classList.toggle('dark');
-    if(document.body.classList.contains("dark")){
-        icon.src="sun.png"
-    }else{
-        icon.src="moon.png"
+    if (document.body.classList.contains("dark")) {
+        icon.src = "./Assets/sun.png";
+    } else {
+        icon.src = "./Assets/moon.png";
     }
-})
+});
--- Fetch.js
+++
@@ -1,8 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-.then(response => response.json())
-.then((data) => {
-    console.log(data);
-})
-.catch((error) =>{
-    console.log(error);
-})
