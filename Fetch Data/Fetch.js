2f62ff1918f2f670340902d41d0b92a124a644fe
minnukota381 <minnukota381@gmail.com>
2024-06-06 20:43:13 +0530

    update | Fetch Data

--- Fetch Data/Fetch.js
+++ Fetch Data/Fetch.js
@@ -1,8 +1,44 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-.then(response => response.json())
-.then((data) => {
-    console.log(data);
-})
-.catch((error) =>{
-    console.log(error);
-})
+document.getElementById("fetchExternal").addEventListener("click", fetchExternalData);
+document.getElementById("fetchLocal").addEventListener("click", fetchLocalData);
+
+function fetchExternalData() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(data => displayData(data))
+        .catch(error => console.error('Error fetching external data:', error));
+}
+
+function fetchLocalData() {
+    fetch("localdata.json")
+        .then(response => response.json())
+        .then(data => displayData(data))
+        .catch(error => console.error('Error fetching local data:', error));
+}
+
+function displayData(data) {
+    const output = document.getElementById("output");
+    let table = `<table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Username</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>`;
+    
+    data.forEach(user => {
+        table += `<tr>
+                    <td>${user.id}</td>
+                    <td>${user.name}</td>
+                    <td>${user.username}</td>
+                    <td>${user.email}</td>
+                  </tr>`;
+    });
+    
+    table += `  </tbody>
+                </table>`;
+    
+    output.innerHTML = table;
+}
