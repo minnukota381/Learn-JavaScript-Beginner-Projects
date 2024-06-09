11910bbc68322267b5673ff0a1a1722599de94c9
minnukota381 <minnukota381@gmail.com>
2024-06-09 18:20:10 +0530

    update | PW Generator

--- Password Generator/PasswordGenerator.js
+++ Password Generator/PasswordGenerator.js
@@ -1,9 +1,9 @@
 function generatePassword() {
     let length = document.getElementById("length").value;
-    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
-    var password = "";
-    for (var i = 0; i < length; i++) {
-      var randomIndex = Math.floor(Math.random() * charset.length);
+    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      let randomIndex = Math.floor(Math.random() * charset.length);
       password += charset[randomIndex];
     }
     document.getElementById("password").value = password;
