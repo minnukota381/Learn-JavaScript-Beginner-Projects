3e4a11b8f6d10a748c91899b8003e11d2f81c1de
minnukota381 <minnukota381@gmail.com>
2024-06-10 13:30:07 +0530

    update | Show/Hide PW

--- Show Hide Password/Password.js
+++ Show Hide Password/Password.js
@@ -1,11 +1,26 @@
+// document.getElementById("togglePassword").addEventListener("click", function() {
+//     var password1=document.getElementById("password");
+//     var toggle=document.getElementById("togglePassword");
+//     if(password1.type==="password"){
+//         password1.type="text";
+//         toggle.innerHTML="Hide";
+//     }else{
+//         password1.type="password";
+//         toggle.innerHTML="show";
+//     }
+// })
+
 document.getElementById("togglePassword").addEventListener("click", function() {
-    var password1=document.getElementById("password");
-    var toggle=document.getElementById("togglePassword");
-    if(password1.type==="password"){
-        password1.type="text";
-        toggle.innerHTML="Hide";
-    }else{
-        password1.type="password";
-        toggle.innerHTML="show";
+    var password = document.getElementById("password");
+    var toggleIcon = document.querySelector("#togglePassword i");
+
+    if (password.type === "password") {
+        password.type = "text";
+        toggleIcon.classList.remove("fa-eye");
+        toggleIcon.classList.add("fa-eye-slash");
+    } else {
+        password.type = "password";
+        toggleIcon.classList.remove("fa-eye-slash");
+        toggleIcon.classList.add("fa-eye");
     }
-})
+});
